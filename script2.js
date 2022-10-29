30d652dca1ebecf974ad8c5d1979dab3139d9c2a
Sam <108341453+Syamsu7@users.noreply.github.com>
2022-10-29 14:01:38 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,3 +7,18 @@ const huruf = [...nama.textContent].map ( (h) => {
 	}).join('');
 
 nama.innerHTML = huruf;
+
+//Self contact info
+
+function email(){
+	alert('muhamadsyamsu6@gmail.com');
+}
+function instagram(){
+	alert('@syamsunuryaman');
+}
+function youTube(){
+	alert('Britishengfeast.');
+}
+function github(){
+	alert('Syamsu7');
+}
